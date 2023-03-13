00ab211543c0a5ade25f52b99fbdd9398a74a4dd
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-13 10:49:38 +0900

    313

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+// 파일명 = 클래스명
+public class HelloWorld{
+    // 프로그램 시작
+    // main 메서드
+    public static void main(String[] args) {
+        // 내용 기술
+        // 출력
+
+        System.out.println("Hello World");
+    }
+}
