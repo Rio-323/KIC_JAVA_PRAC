1fbcbb8e25ced62befc001a9161a4d60c4e58d85
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-16 12:45:36 +0900

    3.16

---
+++ ThisPerson.java
@@ -0,0 +1,19 @@
+public class ThisPerson {
+    String name;
+    int age;
+    boolean isHungry;
+
+    ThisPerson(String name, int age, boolean isHungry) {
+        this.name = name;
+        this.age = age;
+        this.isHungry = isHungry;
+    }
+
+    String getPerssonInfo() {
+        return String.format("name : %s, age : %d, isHungry : %b", this.name, this.age, this.isHungry);
+    }
+    public static void main(String[] args) {
+        ThisPerson person = new ThisPerson("홍길동", 20, true);
+        System.out.println(person.getPerssonInfo());
+    }
+}
