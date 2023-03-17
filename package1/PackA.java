90c5105a70459c6af928ea962bf1cd64d7680207
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-17 15:57:26 +0900

    3.17

---
+++ package1/PackA.java
@@ -0,0 +1,8 @@
+package package1;
+// defualt package
+
+public class PackA {
+    public PackA() {
+        System.out.println("PackA 생성자 호출");
+    }
+}
---
+++ package1/PackMain.java
@@ -0,0 +1,8 @@
+package package1;
+// main 메서드가 존재하는 클래스를 실행 클래스
+
+public class PackMain {
+    public static void main(String[] args) {
+        PackA a = new PackA();
+    }
+}
