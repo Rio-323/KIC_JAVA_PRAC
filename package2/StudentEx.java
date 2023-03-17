a7f49610f1697c01d1b9a0cb21f91108839f19de
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-17 16:46:36 +0900

    3.17

--- package2/StudentEx.java
+++ package2/StudentEx.java
@@ -1,6 +1,9 @@
+import com.naver.Student;
+
 public class StudentEx {
     public static void main(String[] args) {
-        com.naver.Student stu = new com.naver.Student();
+        // com.naver.Student stu = new com.naver.Student();
+        Student stu = new Student();
 
         stu.hakbun = "1001";
         System.out.println(stu.hakbun);
