55807861e737b99894dee4b95230ba3ac4d38763
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-20 11:05:06 +0900

    3.20

--- java-workspace/JavaEx01/src/MainEx01.java
+++ java-workspace/JavaEx01/src/MainEx01.java
@@ -1,9 +1,30 @@
+import pack1.Student2;
 
 public class MainEx01 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Eclipse");
-
+		
+		
+		Student1 stu1 = new Student1();
+		stu1.hakbun = "1001";
+		stu1.name = "홍길동";
+		System.out.println(stu1.hakbun);
+		System.out.println(stu1.name);
+		System.out.println();
+		
+//		Student2 stu2 = new Student2();
+//		stu2.hakbun = "1002";
+//		stu2.name = "이몽룡";
+//		System.out.println(stu2.hakbun);
+//		System.out.println(stu2.name);
+		
+		Student2 stu2 = new Student2();
+		stu2.hakbun = "1002";
+		stu2.name = "이몽룡";
+		System.out.println(stu2.hakbun);
+		System.out.println(stu2.name);
+		
 	}
 
 }
