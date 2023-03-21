10d7ec3c10364fa082ad62e71d86910e6b453080
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-21 11:17:45 +0900

    3.21

--- java-workspace/LangEx02/src/ProcessBuilderEx01.java
+++ java-workspace/LangEx02/src/ProcessBuilderEx01.java
@@ -5,8 +5,7 @@ public class ProcessBuilderEx01 {
 		// ProcessBuilder processBuilder = new ProcessBuilder("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe", "www.google.com");
 		// processBuilder.start();
 		
-		// 메모장 실행시켜보기 %windir%\system32\notepad.exe
-		
+		// 메모장 실행시켜보기		
 		ProcessBuilder processBuilder2 = new ProcessBuilder("C:\\Windows\\System32\\notepad.exe");
 		processBuilder2.start();
 	}
