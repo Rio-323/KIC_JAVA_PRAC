e2796dccef21de8f9632a276989d4c5726adbecf
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-21 16:17:25 +0900

    3.21

---
+++ java-workspace/UtilEx01/src/ScannerEx03.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ScannerEx03 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String msg = "";
+		while(true) {
+			System.out.print("입력 > ");
+			msg = sc.nextLine();
+			
+			if(!msg.equals("exit")) {
+				System.out.println("msg : " + msg);
+			} else {
+				System.out.println("종료");
+				break;
+			}
+		}
+		
+		sc.close();
+
+	}
+
+}
