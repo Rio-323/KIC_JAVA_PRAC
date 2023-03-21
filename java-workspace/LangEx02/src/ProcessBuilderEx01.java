014893a1221394f2365ae099523147ac88d83f8f
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-21 11:16:32 +0900

    3.21

---
+++ java-workspace/LangEx02/src/ProcessBuilderEx01.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+public class ProcessBuilderEx01 {
+
+	public static void main(String[] args) throws IOException {
+		// ProcessBuilder processBuilder = new ProcessBuilder("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe", "www.google.com");
+		// processBuilder.start();
+		
+		// 메모장 실행시켜보기 %windir%\system32\notepad.exe
+		
+		ProcessBuilder processBuilder2 = new ProcessBuilder("C:\\Windows\\System32\\notepad.exe");
+		processBuilder2.start();
+	}
+
+}
