8f21bd7d69ec9088cb84815b0035a52e2d5dae6b
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-23 16:01:46 +0900

    3.23

--- java-workspace/ExceptionEx01/src/ExceptionEx07.java
+++ java-workspace/ExceptionEx01/src/ExceptionEx07.java
@@ -18,11 +18,49 @@ public class ExceptionEx07 {
 		
 		System.out.println("메서드 끝");
 	}
+	
+	public void method2(int num) throws Exception {
+		System.out.println("메서드 시작");
+		
+		if(num >= 100) {
+			System.out.println("100보다 크다");
+		} else {
+			throw new Exception("100보다 작다");
+		}
+		
+		System.out.println("메서드 끝");
+	}
+	
+	public void method3(int num) throws CustomException {
+		System.out.println("메서드 시작");
+		
+		if(num >= 100) {
+			System.out.println("100보다 크다");
+		} else {
+			throw new CustomException("100보다 작다");
+		}
+		
+		System.out.println("메서드 끝");
+	}
 
 	public static void main(String[] args) {
 		ExceptionEx07 ee = new ExceptionEx07();
 		ee.method1(10);
 		ee.method1(200);
+		try {
+			ee.method2(10);
+		} catch(Exception e) {
+			System.out.println("[Error] : " + e.getMessage());
+		}
+		
+		try {
+			ee.method3(10);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			System.out.println("[Error] : " + e.getMessage());
+		}
+		
 	}
 
 }
---
+++ java-workspace/ExceptionEx01/src/ExceptionEx08.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+public class ExceptionEx08 {
+	
+	public static void main(String[] args) {
+		ProcessBuilder processBuilder = new ProcessBuilder("C:\\\\Windows\\\\System32\\\\notepad.exe");
+		
+//		try {
+//			processBuilder.start();
+//		} catch(IOException e) {
+//			System.out.println("[Error] : " + e.getMessage());
+//		}
+		
+//		try {
+//			processBuilder.start();
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//		}
+		
+	}
+
+}
