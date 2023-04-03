88ad398e0a1eb3891ff460eb1b3f633244866147
Rio-323 <kimsiyeon5214@gmail.com>
2023-04-03 14:26:22 +0900

    4.03

---
+++ java-workspace/JDBCEx01/src/JDBCEx04.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCEx04 {
+
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost:3306/sample";
+		String user = "root";
+		String password = "123456";
+		
+		Connection conn = null;
+		
+	
+		try {
+			// 처리로
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			System.out.println("드라이버 로딩 완료");
+			
+			conn = DriverManager.getConnection(url, user, password);
+			System.out.println("연결 성공");
+		} catch (ClassNotFoundException e) {
+			System.out.println("[Error] : " + e.getMessage());
+		} catch (SQLException e) {
+			System.out.println("[Error] : " + e.getMessage());
+		} finally { if(conn != null) try {conn.close();} catch(SQLException e) {System.out.println("[Error] : " + e.getMessage());}}
+	}
+
+}
