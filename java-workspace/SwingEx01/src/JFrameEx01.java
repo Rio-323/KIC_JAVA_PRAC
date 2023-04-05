b5cdcc6c67a2457f26a8a0e113a5cc8f762a528c
Rio-323 <kimsiyeon5214@gmail.com>
2023-04-05 11:55:47 +0900

    4.05

---
+++ java-workspace/SwingEx01/src/JFrameEx01.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class JFrameEx01 {
+
+	public static void main(String[] args) {
+		
+		// has ~ a
+		JFrame frame = new JFrame();
+		frame.setSize(1024, 768);
+		
+		frame.setVisible(true);
+	}
+
+}
