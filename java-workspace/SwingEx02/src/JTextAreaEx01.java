c3072629b3ef60ca5172e5e697a4c4caedc6e538
Rio-323 <kimsiyeon5214@gmail.com>
2023-04-06 10:21:28 +0900

    4.06

--- java-workspace/SwingEx02/src/JTextAreaEx01.java
+++ java-workspace/SwingEx02/src/JTextAreaEx01.java
@@ -4,10 +4,17 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 public class JTextAreaEx01 extends JFrame {
 
 	private JPanel contentPane;
+	private JTextArea textArea;
+	private JScrollPane scrollPane;
 
 	/**
 	 * Launch the application.
@@ -38,9 +45,26 @@ public class JTextAreaEx01 extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JTextArea textArea = new JTextArea();
-		// TextArea -> 여러줄 입력 / Jlabel의 한계점 보완
-		textArea.setBounds(6, 6, 148, 147);
-		contentPane.add(textArea);
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(6, 6, 148, 147);
+		contentPane.add(scrollPane);
+		
+		textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		
+		JButton btn = new JButton("New button");
+		btn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// 지운 다음 다시 쓰기
+				// textArea.setText("new Text");
+				
+				// textArea.append("new Text\n");
+				textArea.append("new Text" + System.lineSeparator());
+				
+			}
+		});
+		btn.setBounds(166, 6, 117, 29);
+		contentPane.add(btn);
 	}
 }
