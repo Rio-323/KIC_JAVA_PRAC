5ae9f43c50d1c10ff1ab90f44f1531e2798e555f
Rio-323 <kimsiyeon5214@gmail.com>
2023-04-07 10:37:53 +0900

    4.07

--- java-workspace/SwingEx02/src/JSliderEx01.java
+++ java-workspace/SwingEx02/src/JSliderEx01.java
@@ -3,10 +3,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JSlider;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
 
 public class JSliderEx01 extends JFrame {
 
 	private JPanel contentPane;
+	private JSlider slider;
+	private JButton btn;
 
 	/**
 	 * Launch the application.
@@ -36,6 +44,29 @@ public class JSliderEx01 extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		slider = new JSlider();
+		slider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				System.out.println(slider.getValue());
+			}
+		});
+		slider.setMaximum(200);
+		slider.setPaintTicks(true);
+		slider.setMinorTickSpacing(5);
+		slider.setMajorTickSpacing(20);
+		slider.setPaintLabels(true);
+		slider.setBounds(6, 17, 542, 58);
+		contentPane.add(slider);
+		
+		btn = new JButton("New button");
+		btn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println(slider.getValue());
+			}
+		});
+		btn.setBounds(6, 101, 117, 29);
+		contentPane.add(btn);
 	}
-
 }
