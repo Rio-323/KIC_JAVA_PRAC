0f7b190a0907fc7fbfcf616b17f70be0023f6e70
Rio-323 <kimsiyeon5214@gmail.com>
2023-04-07 10:54:09 +0900

    4.07

--- java-workspace/SwingEx02/src/JSliderEx01.java
+++ java-workspace/SwingEx02/src/JSliderEx01.java
@@ -9,12 +9,15 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.ChangeEvent;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
 
 public class JSliderEx01 extends JFrame {
 
 	private JPanel contentPane;
 	private JSlider slider;
 	private JButton btn;
+	private JTextArea textArea;
 
 	/**
 	 * Launch the application.
@@ -48,7 +51,12 @@ public class JSliderEx01 extends JFrame {
 		slider = new JSlider();
 		slider.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent e) {
-				System.out.println(slider.getValue());
+				// System.out.println(slider.getValue());
+				// textArea.append(slider.getValue() + System.lineSeparator()); -> NullPointerException -> 생성 시점에서의 차이 -> textArea = new JTextArea(); -> 아래에 선언이 되어 있어서 발생.
+				
+				if(textArea != null) {
+					textArea.append(slider.getValue() + System.lineSeparator());
+				}
 			}
 		});
 		slider.setMaximum(200);
@@ -68,5 +76,13 @@ public class JSliderEx01 extends JFrame {
 		});
 		btn.setBounds(6, 101, 117, 29);
 		contentPane.add(btn);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(16, 149, 532, 270);
+		contentPane.add(scrollPane);
+		
+		textArea = new JTextArea();
+		textArea.setEditable(false);
+		scrollPane.setViewportView(textArea);
 	}
 }
