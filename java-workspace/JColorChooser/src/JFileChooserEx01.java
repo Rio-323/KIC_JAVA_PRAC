b48ead31b5602c4df8481cee022bb2027b222cd1
Rio-323 <kimsiyeon5214@gmail.com>
2023-04-11 12:28:46 +0900

    4.11

--- java-workspace/JColorChooser/src/JFileChooserEx01.java
+++ java-workspace/JColorChooser/src/JFileChooserEx01.java
@@ -1,16 +1,16 @@
-import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
-import javax.swing.JColorChooser;
+import javax.swing.JFileChooser;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
 
-public class JColorChooserEx01 extends JFrame {
+public class JFileChooserEx01 extends JFrame {
 
 	private JPanel contentPane;
 
@@ -21,7 +21,7 @@ public class JColorChooserEx01 extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					JColorChooserEx01 frame = new JColorChooserEx01();
+					JFileChooserEx01 frame = new JFileChooserEx01();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -33,7 +33,7 @@ public class JColorChooserEx01 extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public JColorChooserEx01() {
+	public JFileChooserEx01() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 600);
@@ -43,24 +43,27 @@ public class JColorChooserEx01 extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btn1 = new JButton("New button");
-		btn1.addMouseListener(new MouseAdapter() {
+		JButton btn = new JButton("New button");
+		btn.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				Color color = JColorChooser.showDialog(JColorChooserEx01.this, "파란색", Color.blue);
-				System.out.println("결과 : " + color);
+				JFileChooser fileChooser = new JFileChooser("C:\\");
+				int result = fileChooser.showOpenDialog(JFileChooserEx01.this);
 				
-				if(color != null) {
-					System.out.println("Red : " + color.getRed());
-					System.out.println("Blue : " + color.getBlue());
-					System.out.println("Green : " + color.getGreen());
-				} else {
-					System.out.println("취소 선택");
+				if(result == JFileChooser.APPROVE_OPTION) {
+					System.out.println("확인");
+					System.out.println(fileChooser.getSelectedFile().getName());
+					File file = fileChooser.getSelectedFile();
+					System.out.println(file.getName());
+					System.out.println(file.getAbsolutePath());
+				} else if(result == JFileChooser.CANCEL_OPTION) {
+					System.out.println("취소");
 				}
+				
 			}
 		});
-		btn1.setBounds(6, 6, 117, 29);
-		contentPane.add(btn1);
+		btn.setBounds(6, 6, 117, 29);
+		contentPane.add(btn);
 	}
 
 }
