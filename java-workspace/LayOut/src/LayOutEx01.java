1e14292dd98c5acb9d70dc7cbc4207821a69369e
Rio-323 <kimsiyeon5214@gmail.com>
2023-04-12 14:12:10 +0900

    4.12

--- java-workspace/LayOut/src/LayOutEx01.java
+++ java-workspace/LayOut/src/LayOutEx01.java
@@ -6,10 +6,16 @@ import javax.swing.border.EmptyBorder;
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
 import javax.swing.JButton;
+import java.awt.GridLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class LayOutEx01 extends JFrame {
 
 	private JPanel contentPane;
+	private JPanel panel2;
 
 	/**
 	 * Launch the application.
@@ -39,20 +45,70 @@ public class LayOutEx01 extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		JButton btnNewButton = new JButton("North");
-		contentPane.add(btnNewButton, BorderLayout.NORTH);
+		JPanel panel1 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel1.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		contentPane.add(panel1, BorderLayout.NORTH);
 		
-		JButton btnNewButton_1 = new JButton("South");
-		contentPane.add(btnNewButton_1, BorderLayout.SOUTH);
+		JButton btn1 = new JButton("Panel1");
+		btn1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// name_65989100413583
+				((CardLayout)panel2.getLayout()).show(panel2, "name_65989100413583");
+			}
+		});
+		panel1.add(btn1);
 		
-		JButton btnNewButton_2 = new JButton("West");
-		contentPane.add(btnNewButton_2, BorderLayout.WEST);
+		JButton btn2 = new JButton("Panel2");
+		btn2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// name_66192192133041
+				((CardLayout)panel2.getLayout()).show(panel2, "name_66192192133041");
+			}
+		});
+		panel1.add(btn2);
 		
-		JButton btnNewButton_3 = new JButton("East");
-		contentPane.add(btnNewButton_3, BorderLayout.EAST);
+		JButton btn3 = new JButton("Panel3");
+		btn3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// name_66052913226541
+				((CardLayout)panel2.getLayout()).show(panel2, "name_66052913226541");
+			}
+		});
+		panel1.add(btn3);
 		
-		JButton btnNewButton_4 = new JButton("Center");
-		contentPane.add(btnNewButton_4, BorderLayout.CENTER);
+		panel2 = new JPanel();
+		contentPane.add(panel2, BorderLayout.CENTER);
+		panel2.setLayout(new CardLayout(0, 0));
+		
+		JPanel panel21 = new JPanel();
+		panel21.setBackground(new Color(255, 0, 12));
+		panel2.add(panel21, "name_65989100413583");
+		panel21.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(6, 6, 117, 29);
+		panel21.add(btnNewButton);
+		
+		JPanel panel22 = new JPanel();
+		panel2.add(panel22, "name_66192192133041");
+		panel22.setBackground(new Color(16, 10, 233));
+		panel22.setLayout(null);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(6, 6, 117, 29);
+		panel22.add(btnNewButton_1);
+		
+		JPanel panel23 = new JPanel();
+		panel23.setBackground(new Color(0, 223, 10));
+		panel2.add(panel23, "name_66052913226541");
+		panel23.setLayout(null);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(6, 6, 117, 29);
+		panel23.add(btnNewButton_2);
 	}
-
 }
