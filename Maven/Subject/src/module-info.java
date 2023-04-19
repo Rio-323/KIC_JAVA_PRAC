e8fd0b7ac49a06822a1f7e94f323a69eefe0bcc8
Rio-323 <kimsiyeon5214@gmail.com>
2023-04-19 09:16:36 +0900

    4.19

--- Maven/Subject/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author ksy
- *
- */
-module Subject {
-}
--- Maven/test/src/main/java/com/exam/test/test/App.java
+++
@@ -1,8 +0,0 @@
-package com.exam.test.test;
-
-public class App 
-{
-	public static void main(String[] args) {
-		
-	}
-}
