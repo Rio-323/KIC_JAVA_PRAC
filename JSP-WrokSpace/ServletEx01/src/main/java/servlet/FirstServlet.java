cba0005c14dc6a05857dd3abc8778b4bf463aa77
Rio-323 <kimsiyeon5214@gmail.com>
2023-05-02 14:54:29 +0900

    5.02

--- JSP-WrokSpace/ServletEx01/src/main/java/servlet/FirstServlet.java
+++ JSP-WrokSpace/ServletEx01/src/main/java/servlet/FirstServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +12,20 @@ public class FirstServlet extends HttpServlet {
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("Hello Servlet");
+		// System.out.println("Hello Servlet");
+		resp.setContentType("text/html; charset=utf-8");
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset='utf-8'>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("Hello Servlet");
+		out.println("</body>");
+		out.println("</html>");
+		
+		out.close();
 	}
 
 }
