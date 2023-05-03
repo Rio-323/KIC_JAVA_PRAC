3e4c0d3dce6b1356efec09ec482c584361b09c3b
Rio-323 <kimsiyeon5214@gmail.com>
2023-05-03 11:54:54 +0900

    5.03

---
+++ JSP-WrokSpace/ParameterEx01/src/main/java/model2/View1Action.java
@@ -0,0 +1,10 @@
+package model2;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class View1Action {
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		
+	}
+}
