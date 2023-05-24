de87af3f5ee4d32e05a93127883a516a1e3e5f39
Rio-323 <kimsiyeon5214@gmail.com>
2023-05-24 14:31:43 +0900

    5.24

--- Spring-Workspace/ex01/src/main/java/com/exam/spring01/HelloBean1.java
+++ Spring-Workspace/ex01/src/main/java/com/exam/spring01/HelloBean1.java
@@ -2,7 +2,7 @@ package com.exam.spring01;
 
 public class HelloBean1 {
 	
-	public void syHello(String name) {
+	public void sayHello(String name) {
 		System.out.println(name + "님 안녕하세요.");
 	}
 
---
+++ Spring-Workspace/ex01/src/main/java/com/exam/spring01/HelloBean2.java
@@ -0,0 +1,9 @@
+package com.exam.spring01;
+
+public class HelloBean2 {
+	
+	public void sayHello(String name) {
+		System.out.println("Hello" + name);
+	}
+
+}
