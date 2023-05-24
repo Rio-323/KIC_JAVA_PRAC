f1573d658706dcb855d11473fd68a54f15f0d747
Rio-323 <kimsiyeon5214@gmail.com>
2023-05-24 14:33:06 +0900

    5.24

---
+++ Spring-Workspace/ex01/src/main/java/com/exam/spring01/App.java
@@ -0,0 +1,13 @@
+package com.exam.spring01;
+
+public class App {
+
+	public static void main(String[] args) {
+		HelloBean1 helloBean1 = new HelloBean1();
+		helloBean1.sayHello("홍길동");
+		
+		HelloBean2 helloBean2 = new HelloBean2();
+		helloBean2.sayHello("박문수");
+	}
+
+}
