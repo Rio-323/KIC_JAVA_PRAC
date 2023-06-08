40827baf5411cf5164d150051fb8597e5c533708
Rio-323 <kimsiyeon5214@gmail.com>
2023-06-08 09:21:19 +0900

    6.08

---
+++ SpringBoot/BootJdbcTemplateBoard/src/main/java/com/example/bootjdbctemplateboard/BootJdbcTemplateBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootjdbctemplateboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJdbcTemplateBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJdbcTemplateBoardApplication.class, args);
+	}
+
+}
