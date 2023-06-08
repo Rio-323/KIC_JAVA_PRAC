e378ae2b7c54251ec2c40c144b891c682fe94eae
Rio-323 <kimsiyeon5214@gmail.com>
2023-06-08 09:08:10 +0900

    6.08

--- SpringBoot/ex07/src/main/java/com/example/model/ZipcodeDAO.java
+++ SpringBoot/ex07/src/main/java/com/example/model/ZipcodeDAO.java
@@ -3,6 +3,7 @@ package com.example.model;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +13,11 @@ public class ZipcodeDAO {
 	@Autowired
 	private SqlSession sqlSession;
 	
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
+	
 	public List<ZipcodeTO> zipcodeLists(String strDong) {
+		sqlSession = sqlSessionFactory.openSession();
 		
 		List<ZipcodeTO> lists = sqlSession.selectList("selectzipcode", strDong + "%");
 		
