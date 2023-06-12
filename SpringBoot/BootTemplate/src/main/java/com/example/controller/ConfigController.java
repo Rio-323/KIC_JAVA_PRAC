fd7bd5fbb5a3960ee269302be0d50de78a54f6dd
Rio-323 <kimsiyeon5214@gmail.com>
2023-06-12 16:31:29 +0900

    6.12

--- SpringBoot/BootTemplate/src/main/java/com/example/controller/ConfigController.java
+++ SpringBoot/BootTemplate/src/main/java/com/example/controller/ConfigController.java
@@ -1,8 +1,12 @@
 package com.example.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.example.model.BoardTO;
 
 @RestController
 public class ConfigController {
@@ -15,5 +19,32 @@ public class ConfigController {
 	@RequestMapping("/view1.do")
 	public ModelAndView view1() {
 		return new ModelAndView("view1");
+	}
+	
+	@RequestMapping("/view2.do")
+	public ModelAndView view2() {
+		BoardTO to = new BoardTO();
+		to.setSeq("10");
+		to.setSubject("제목 10");
+		
+		ArrayList<BoardTO> lists = new ArrayList<>();
+		BoardTO to1 = new BoardTO();
+		to1.setSeq("20");
+		to1.setSubject("제목 20");
+		
+		BoardTO to2 = new BoardTO();
+		to2.setSeq("30");
+		to2.setSubject("제목 30");
+		
+		lists.add(to1);
+		lists.add(to2);
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("view2");
+		modelAndView.addObject("data1", "값 1");
+		modelAndView.addObject("to", to);
+		modelAndView.addObject("lists", lists);
+		
+		return modelAndView;
 	}
 }
---
+++ SpringBoot/BootTemplate/src/main/java/com/example/model/BoardTO.java
@@ -0,0 +1,11 @@
+package com.example.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardTO {
+	private String seq;
+	private String subject;	
+}
